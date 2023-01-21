1d97814547b69a61f391c4fd6869675735631001
cindy <remycindy@gmail.com>
2023-01-21 12:24:52 +0100

    creation formulaire livre

---
+++ src/Form/LivreType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Genre;
+use App\Entity\Livre;
+use Doctrine\DBAL\Types\DateType;
+use Doctrine\DBAL\Types\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LivreType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre',TextType::class)
+            ->add('resume',TextareaType::class)
+            ->add('auteur',TextType::class)
+            ->add('dateAjout',DateType::class)
+            ->add('dateParution',DateType::class)
+            ->add('genre',EntityType::class,[
+                'class'=>Genre::class,
+                'choice_label'=>'nom',
+                'multiple' => false,
+                'expanded' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Livre::class,
+        ]);
+    }
+}
