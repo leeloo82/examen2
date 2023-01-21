d338bd4616c7cc4558a2a1c63c87c63fc3ef9cc6
cindy <remycindy@gmail.com>
2023-01-21 12:53:33 +0100

    creation formulaire + creation fonction de gestion formulaire livre + creation de la vue formulaire livre

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -2,19 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Livre;
+use App\Form\LivreType;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LivreController extends AbstractController
 {
     /**
-     * @Route("/livre", name="app_livre")
+     * @Route("/livre/formulaire", name="app_livre_formulaire")
      */
-    public function index(): Response
+    public function formLivre(EntityManagerInterface $entityManager,Request $request): Response
     {
-        return $this->render('livre/index.html.twig', [
-            'controller_name' => 'LivreController',
+
+        //creation d'un new objet livre
+        $livre = new Livre();
+        //creation de la date du jour dans l'objet livre
+        $livre ->setDateAjout(new DateTime());
+        //creation du formulaire
+        $formLivre = $this->createForm(LivreType::class, $livre);
+
+        $formLivre->handleRequest($request);
+
+        if ($formLivre->isSubmitted() && $formLivre->isValid()) {
+            $livre = $formLivre->getData();
+
+            //envoie du formulaire
+            $entityManager->persist($livre);
+            $entityManager->flush();
+            //redirection vers une vue
+            //return $this->redirectToRoute('app_genre');
+        }
+        //creation de la vue
+        //creation de la vue
+        return $this->render('livre/formLivre.html.twig', [
+            'formLivre' => $formLivre->createView()
         ]);
     }
+
 }
--- src/Form/LivreType.php
+++ src/Form/LivreType.php
@@ -4,8 +4,9 @@ namespace App\Form;
 
 use App\Entity\Genre;
 use App\Entity\Livre;
-use Doctrine\DBAL\Types\DateType;
-use Doctrine\DBAL\Types\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
