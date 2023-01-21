4de63f92e1e6ee9be4e2c53a1eddccb1494f4a89
cindy <remycindy@gmail.com>
2023-01-21 13:26:04 +0100

    gestion page home listing livre

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Livre;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Doctrine\ORM\EntityManagerInterface;
 class HomeController extends AbstractController
 {
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $repository =$entityManager->getRepository(Livre::class);
+        $livre = $repository->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'list_livre' => $livre,
         ]);
     }
 }
